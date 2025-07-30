b56eb961668f46999ac84c4b81d38960a34a5292
saffr3n <saffr3n@gmail.com>
2025-07-30 15:37:03 +0300

    feat: add knightMoves() function

--- index.js
+++ index.js
@@ -25,3 +25,36 @@ class Cell {
     return res;
   }
 }
+
+function knightMoves(start, end) {
+  if ([...start, ...end].some((n) => n < 0 || n > 7)) {
+    throw new Error('Min position: 0, 0; Max position: 7, 7');
+  }
+
+  const queue = [new Cell(start)];
+  const visited = new Set([String(start)]);
+  let res;
+  while (queue.length) {
+    const curr = queue.shift();
+    if (curr.pos[0] === end[0] && curr.pos[1] === end[1]) {
+      res = curr;
+      break;
+    }
+    for (const [dx, dy] of MOVES) {
+      const nextPos = [curr.pos[0] + dx, curr.pos[1] + dy];
+      const key = String(nextPos);
+      if (
+        visited.has(key) ||
+        nextPos[0] < 0 ||
+        nextPos[0] > 7 ||
+        nextPos[1] < 0 ||
+        nextPos[1] > 7
+      ) {
+        continue;
+      }
+      visited.add(key);
+      queue.push(new Cell(nextPos, curr));
+    }
+  }
+  console.log(res.toString());
+}
