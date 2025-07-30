0612478f3966f15370cca5b3d93505ad0fc8cc86
saffr3n <saffr3n@gmail.com>
2025-07-30 15:34:42 +0300

    feat: add Cell class

--- index.js
+++ index.js
@@ -8,3 +8,20 @@ const MOVES = [
   [1, -2],
   [2, -1],
 ];
+
+class Cell {
+  constructor(pos, parent = null) {
+    this.pos = pos;
+    this.parent = parent;
+  }
+
+  toString() {
+    let res = String(this.pos);
+    let parent = this.parent;
+    while (parent) {
+      res = String(parent.pos) + ' -> ' + res;
+      parent = parent.parent;
+    }
+    return res;
+  }
+}
